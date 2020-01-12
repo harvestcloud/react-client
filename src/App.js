ef02b4404d99a46a2e91687a0865372761f81880
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2020-01-12 11:13:33 -0500

    Outline for what does the server say

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 
+function WhatDoesTheServerSay() {
+  return (
+    <div className="what-does-the-server-say">
+      <h2>
+        What does the <span className="fox">fox</span> <em>server</em> say?
+      </h2>
+      <div className="alert alert-success">
+        <pre>Hello</pre>
+        Hello
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-        <h1>Harvest Cloud</h1>
-        <hr />
+      <h1>Harvest Cloud</h1>
+      <hr />
+      <WhatDoesTheServerSay />
     </div>
   );
 }
